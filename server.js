5f2272b120cfe6d674780da8b939b49ca0717341
Renzo Carpio Ponce <me@renzocarpio.com>
2016-05-24 21:46:58 -0500

    como servir archivos estaticos y utilizar pug para renderizar vistas

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine','pug'); //seteando el pug como gestor de vistas
+app.use(express.static('public'));
+
 app.get('/', function(req,res){
-	res.send('Hola mundo!');
+	//res.send('Hola mundo!');
+	res.render('index'); // por defecto busca en views
 });
 
 app.listen(3000, function(err){
